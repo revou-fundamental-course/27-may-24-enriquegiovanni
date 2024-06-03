1034b429ad22b34bf07e96f3284f611cb80abbd6
enriquegiovanni <djou707@gmail.com>
2024-06-04 01:19:01 +0700

    inisiasi config

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-document.getElementById("myForm").addEventListener("submit", function(event){
-    event.preventDefault();
-    alert("Form telah disubmit!");
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Mencegah form melakukan reload halaman
+
+  const name = document.getElementById('name').value;
+  const birthdate = document.getElementById('birthdate').value;
+  const gender = document.querySelector('input[name="gender"]:checked').value;
+  const message = document.getElementById('message').value;
+
+  const result = `Nama: ${name}\nTanggal Lahir: ${birthdate}\nJenis Kelamin: ${gender}\nPesan: ${message}`;
+
+  alert(result);
 });
